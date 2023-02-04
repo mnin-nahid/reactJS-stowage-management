d697fdfe7a0f2c803d7c3bb5616bd82b33e955d0
mnin-nahid <nahidulislam.nayan54@gmail.com>
2023-02-04 20:26:09 +0600

    implement articals part

--- client-side/src/pages/Articals/index.jsx
+++ client-side/src/pages/Articals/index.jsx
@@ -1,11 +1,47 @@
 import React from 'react';
 import Navbar from '../../components/Navbar'
+import './index.scss'
 
 export default function index() {
   return (
     <div>
       <Navbar />
-      <h1>Hello from Articals</h1>
+      <section className='articals'>
+        <div className="container">
+          <h3 id='stowage-articals'>Wallcome to STOWAGE articals !</h3>
+          <article>
+            <h3>React Router enables "client side routing".</h3>
+            <p>In traditional websites, the browser requests a document from a web server, downloads and evaluates CSS and JavaScript assets, and renders the HTML sent from the server. When the user clicks a link, it starts the process all over again for a new page.
+
+              Client side routing allows your app to update the URL from a link click without making another request for another document from the server. Instead, your app can immediately render some new UI and make data requests with fetch to update the page with new information.</p>
+          </article>
+          <article>
+            <h3>React Router enables "client side routing".</h3>
+            <p>In traditional websites, the browser requests a document from a web server, downloads and evaluates CSS and JavaScript assets, and renders the HTML sent from the server. When the user clicks a link, it starts the process all over again for a new page.
+
+              Client side routing allows your app to update the URL from a link click without making another request for another document from the server. Instead, your app can immediately render some new UI and make data requests with fetch to update the page with new information.</p>
+          </article>
+          <article>
+            <h3>React Router enables "client side routing".</h3>
+            <p>In traditional websites, the browser requests a document from a web server, downloads and evaluates CSS and JavaScript assets, and renders the HTML sent from the server. When the user clicks a link, it starts the process all over again for a new page.
+
+              Client side routing allows your app to update the URL from a link click without making another request for another document from the server. Instead, your app can immediately render some new UI and make data requests with fetch to update the page with new information.</p>
+          </article>
+          <article>
+            <h3>React Router enables "client side routing".</h3>
+            <p>In traditional websites, the browser requests a document from a web server, downloads and evaluates CSS and JavaScript assets, and renders the HTML sent from the server. When the user clicks a link, it starts the process all over again for a new page.
+
+              Client side routing allows your app to update the URL from a link click without making another request for another document from the server. Instead, your app can immediately render some new UI and make data requests with fetch to update the page with new information.</p>
+          </article>
+          <article>
+            <h3>React Router enables "client side routing".</h3>
+            <p>In traditional websites, the browser requests a document from a web server, downloads and evaluates CSS and JavaScript assets, and renders the HTML sent from the server. When the user clicks a link, it starts the process all over again for a new page.
+
+              Client side routing allows your app to update the URL from a link click without making another request for another document from the server. Instead, your app can immediately render some new UI and make data requests with fetch to update the page with new information.</p>
+          </article>
+        </div>
+
+      </section>
     </div>
   )
 }
