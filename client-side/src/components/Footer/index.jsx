2c9da5e793b5ad97d61a5b0f6f5fa576f546ff2a
mnin-nahid <nahidulislam.nayan54@gmail.com>
2023-02-05 16:47:45 +0600

    install react-icon and mirror change on footer

--- client-side/src/components/Footer/index.jsx
+++ client-side/src/components/Footer/index.jsx
@@ -1,27 +1,30 @@
 import React from 'react'
 import './index.scss'
+import {MdRoom} from 'react-icons/md'
+import {FiPhoneCall} from 'react-icons/fi'
+import {FaFacebook, FaYoutube, FaInstagram} from 'react-icons/fa'
 
 export default function index() {
     return (
         <>
             <footer>
                 <div className='footer-container'>
-                    <div>
-                        <h3>STOWAGE.com</h3>
+                    <div className='footer-items'>
+                        <h3>STOWAGE.COM</h3>
                         <p>Dolor sit amet consectetur adipisicing elit. Voluptate voluptas sequi amet harum atque ad nisi, quaerat fugit officiis consectetur.</p>
                     </div>
-                    <div>
+                    <div className='footer-items'>
                      
-                            <span>Mirpur, 1216- Dhaka, Bangladesh</span>
-                            <span>Contract: +8801711xxxxx8</span>
+                            <span><MdRoom/> Mirpur, 1216- Dhaka, Bangladesh</span>
+                            <span><FiPhoneCall/> Contract: +8801711-xxxxx8</span>
                         
                     </div>
-                    <div>
+                    <div className='footer-items'>
                         <h3>Social Media</h3>
-                        <div>
-                            <a target={'_blank'} href="https://www.facebook.com">F</a>
-                            <a target={'_blank'} href="https://www.facebook.com">F</a>
-                            <a target={'_blank'} href="https://www.facebook.com">F</a>
+                        <div className='social-links'>
+                            <a target={'_blank'} href="https://www.facebook.com"><FaFacebook/></a>
+                            <a target={'_blank'} href="https://www.facebook.com"><FaYoutube/></a>
+                            <a target={'_blank'} href="https://www.facebook.com"><FaInstagram/></a>
                         </div>
                     </div>
                 </div>
