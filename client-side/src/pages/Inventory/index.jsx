7548cf140516f9fcd48325ddea8587e63e8f0eca
mnin-nahid <nahidulislam.nayan54@gmail.com>
2023-02-08 01:17:32 +0600

    implement simple modal

--- client-side/src/pages/Inventory/index.jsx
+++ client-side/src/pages/Inventory/index.jsx
@@ -1,12 +1,36 @@
-import React from 'react';
 import Navbar from '../../components/Navbar'
 import Footer from '../../components/Footer'
 import './index.scss'
+import { React, useState } from 'react';
+// import Modal from './Modal'
 
 export default function index() {
+  const [open, setOpen] = useState(false)
+  const styles = {
+    popup: {
+      display: open ? "flex" : "none",
+      opacity: open ? "1" : "0",
+    }
+  };
+
   return (
     <div className='inventory-section'>
       <Navbar />
+      <div className="modal-container popup" style={styles.popup}>
+
+        <div className="modal">
+          <h4>This is a modal</h4>
+          <div className="input-feld">
+            <input type="text" name="" id="" placeholder='Name of Product' />
+            <input type="number" name="" id="" placeholder='Product ID' />
+            <input type="number" name="" id="" placeholder='Product quantity' />
+            <input type="submit" value="Submit" />
+          </div>
+        </div>
+        <div className="close-button">
+          <button className="close_button" onClick={() => { setOpen(false) }}>X</button>
+        </div>
+      </div>
       <section>
         <h3>Wallcome to Inventory</h3>
         <div className="container">
@@ -37,13 +61,14 @@ export default function index() {
               <div className="item">Item</div>
             </div>
           </div>
-          <div className='button-container'> 
-            <button>Add</button>
-            <button>Manage</button>
-            <button>Delever</button>
+          <div className='button-container'>
+            <button onClick={() => { setOpen(true) }}>Add</button>
+            <button onClick={() => { setOpen(true) }}>Manage</button>
+            <button onClick={() => { setOpen(true) }}>Delever</button>
           </div>
         </div>
       </section>
+
       <Footer />
     </div>
   )
