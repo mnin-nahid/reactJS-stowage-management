cb87fd8dbb3927c96c75a14f16afd3d2d580fea7
mnin-nahid <nahidulislam.nayan54@gmail.com>
2023-02-07 16:45:25 +0600

    implement inventory simple design

--- client-side/src/pages/Inventory/index.jsx
+++ client-side/src/pages/Inventory/index.jsx
@@ -1,12 +1,49 @@
 import React from 'react';
 import Navbar from '../../components/Navbar'
 import Footer from '../../components/Footer'
+import './index.scss'
 
 export default function index() {
   return (
-    <div>
+    <div className='inventory-section'>
       <Navbar />
-      <h1>Welcome to Inventory</h1>
+      <section>
+        <h3>Wallcome to Inventory</h3>
+        <div className="container">
+          <div className='items-container'>
+            <h4>List of all items</h4>
+            <div className="items">
+              <div className="item">Item</div>
+              <div className="item">Item</div>
+              <div className="item">Item</div>
+              <div className="item">Item</div>
+              <div className="item">Item</div>
+              <div className="item">Item</div>
+              <div className="item">Item</div>
+              <div className="item">Item</div>
+              <div className="item">Item</div>
+              <div className="item">Item</div>
+              <div className="item">Item</div>
+              <div className="item">Item</div>
+              <div className="item">Item</div>
+              <div className="item">Item</div>
+              <div className="item">Item</div>
+              <div className="item">Item</div>
+              <div className="item">Item</div>
+              <div className="item">Item</div>
+              <div className="item">Item</div>
+              <div className="item">Item</div>
+              <div className="item">Item</div>
+              <div className="item">Item</div>
+            </div>
+          </div>
+          <div className='button-container'> 
+            <button>Add</button>
+            <button>Manage</button>
+            <button>Delever</button>
+          </div>
+        </div>
+      </section>
       <Footer />
     </div>
   )
